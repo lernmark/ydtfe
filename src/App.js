50452cc00b8af2051d976fe32475db395eabe6d0
Paul Lernmark <lernmark@gmail.com>
2021-04-22 21:17:37 +0000

    replaced == with ===

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ class App extends Component {
   renderItems = () => {
     const { viewCompleted } = this.state;
     const newItems = this.state.todoList.filter(
-      (item) => item.completed == viewCompleted
+      (item) => item.completed === viewCompleted
     );
 
     return newItems.map((item) => (
